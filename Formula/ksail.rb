10ae3496e3cb71edc93940f5e2666c9875d6dd6f
Nikolai Emil Damm <ned@devantler.com>
2025-02-23 00:54:52 +0100

    Update ksail.rb

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -1,20 +1,23 @@
 class Ksail < Formula
   desc "SDK for Kubernetes"
   homepage "https://github.com/devantler-tech/ksail"
-  sha256 "93fe6c61797468e1c92a240a735a5b442421f9977c959d12e434c79e8174d0fc"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-darwin-arm64"
+      sha256 "93fe6c61797468e1c92a240a735a5b442421f9977c959d12e434c79e8174d0fc"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-darwin-amd64"
+      sha256 "93fe6c61797468e1c92a240a735a5b442421f9977c959d12e434c79e8174d0fc"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-linux-arm64"
+      sha256 "93fe6c61797468e1c92a240a735a5b442421f9977c959d12e434c79e8174d0fc"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-linux-amd64"
+      sha256 "93fe6c61797468e1c92a240a735a5b442421f9977c959d12e434c79e8174d0fc"
     end
   end
 

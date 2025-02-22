890ebb7305e9b172e8afb0e3fbae4d016df93379
Nikolai Emil Damm <neq@energinet.dk>
2025-02-23 00:23:34 +0100

    fix: update sha256 for ksail binaries in macOS and Linux formula
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -2,22 +2,19 @@ class Ksail < Formula
   desc "SDK for Kubernetes"
   homepage "https://github.com/devantler-tech/ksail"
   license "MIT"
+  sha256 "12a19ca431019f99596fbbb3b6e53197b9374efa9f886c09ca5493581cec8913"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-darwin-arm64"
-      sha256 "12a19ca431019f99596fbbb3b6e53197b9374efa9f886c09ca5493581cec8913"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-darwin-amd64"
-      sha256 "12a19ca431019f99596fbbb3b6e53197b9374efa9f886c09ca5493581cec8913"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-linux-arm64"
-      sha256 "12a19ca431019f99596fbbb3b6e53197b9374efa9f886c09ca5493581cec8913"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.5.0/ksail-linux-amd64"
-      sha256 "12a19ca431019f99596fbbb3b6e53197b9374efa9f886c09ca5493581cec8913"
     end
   end
 

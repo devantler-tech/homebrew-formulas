b9774f49ba4d193ec40d34373ef33710f03c6d81
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-24 22:51:45 +0200

    fix: add livecheck block for macOS and Linux binaries in ksail formula
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -4,15 +4,16 @@ class Ksail < Formula
 
   license "Apache-2.0"
 
-  livecheck do
-    url "https://github.com/devantler-tech/ksail/releases"
-    regex(/href=.*?\/tag\/v?(\d+(?:\.\d+)+)["' >]/i)
-  end
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.25.1/ksail-darwin-amd64"
       sha256 "3af40b508d70813e461a2d8cfddbc11abc576643c775419c289669cd55394fec"
+
+      livecheck do
+        url :stable
+        regex(/^v?(\d+(?:\.\d+)+)$/i)
+      end
 
       def install
         bin.install "ksail-darwin-amd64" => "ksail"
@@ -21,6 +22,11 @@ class Ksail < Formula
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.25.1/ksail-darwin-arm64"
       sha256 "53523e6828d3a028277466fc2c2460b309ba0334b9d150e3d86a69a393525112"
+
+      livecheck do
+        url :stable
+        regex(/^v?(\d+(?:\.\d+)+)$/i)
+      end
 
       def install
         bin.install "ksail-darwin-arm64" => "ksail"
@@ -32,6 +38,11 @@ class Ksail < Formula
       url "https://github.com/devantler-tech/ksail/releases/download/v2.25.1/ksail-linux-amd64"
       sha256 "6eff03768f5faaa9357c1184436be8f5c2c8066740c701a5b3d643b3b3cfb60d"
 
+      livecheck do
+        url :stable
+        regex(/^v?(\d+(?:\.\d+)+)$/i)
+      end
+
       def install
         bin.install "ksail-linux-amd64" => "ksail"
       end
@@ -39,6 +50,11 @@ class Ksail < Formula
     if Hardware::CPU.arm?
       url "https://github.com/devantler-tech/ksail/releases/download/v2.25.1/ksail-linux-arm64"
       sha256 "facf32da03be935b89d86a17dfda973c4810fb080cb884c0601cbe6e40e66382"
+
+      livecheck do
+        url :stable
+        regex(/^v?(\d+(?:\.\d+)+)$/i)
+      end
 
       def install
         bin.install "ksail-linux-arm64" => "ksail"

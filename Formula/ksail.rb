cc904de744f8be9b35c2a55b40431de00e5b0253
Anka <runner@Mac-1746353292289.local>
2025-05-04 10:38:00 +0000

    ksail 2.30.7

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -1,8 +1,8 @@
 class Ksail < Formula
   desc "SDK for Kubernetes"
   homepage "https://github.com/devantler-tech/ksail"
-  url "https://github.com/devantler-tech/ksail/releases/download/v2.30.4/ksail.tar.gz"
-  sha256 "247fd5042210b02d9a274317dd1fde7273b915069411acf83927640703afe564"
+  url "https://github.com/devantler-tech/ksail/releases/download/v2.30.7/ksail.tar.gz"
+  sha256 "7e27d9ceff21802c4873ffe5810ba954a1010d64aca292ebbc6baa04ab27ae25"
   license "Apache-2.0"
 
   livecheck do

9846cd7f6cf84b27b0ea157c499472d33e192814
Nikolai Emil Damm <ned@devantler.com>
2025-05-22 08:04:27 +0200

    ksail 2.31.9 (#283)
    Co-authored-by: Anka <runner@Mac-1747892263364.local>

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -1,8 +1,8 @@
 class Ksail < Formula
   desc "SDK for Kubernetes"
   homepage "https://github.com/devantler-tech/ksail"
-  url "https://github.com/devantler-tech/ksail/releases/download/v2.31.4/ksail.tar.gz"
-  sha256 "d4a71e60ac21a27a07826f2fabdb3592f05e4f31e5938c660c8f0d4a9b4a1bd6"
+  url "https://github.com/devantler-tech/ksail/releases/download/v2.31.9/ksail.tar.gz"
+  sha256 "ed51088e5ec2228fa0bfa0fe7e5780d9f1c81f7264d007cc786179fd0668f344"
   license "Apache-2.0"
 
   livecheck do
